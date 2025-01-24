6eec3f02326163eb0b2a90e1341bcc468ad44b47
Steve <stevowny@gmail.com>
2025-01-24 17:11:09 +0300

    init and learn firebase

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,37 @@
-import Image from "next/image";
+"use client";
+import { auth } from "@/libs/firebaseConfig";
+import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 
-export default function Home() {
+export default function Auth() {
+  const provider = new GoogleAuthProvider();
+
+  const handleSignIn = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      console.log("User:", result.user);
+    } catch (error) {
+      console.error("Error signing in:", error);
+    }
+  };
+
+  const handleSignOut = async () => {
+    try {
+      await signOut(auth);
+      console.log("Signed out successfully");
+    } catch (error) {
+      console.error("Error signing out:", error);
+    }
+  };
+
   return (
-    <div className="">
-      firebase
+    <div>
+      <button onClick={handleSignIn} className="bg-blue-500 text-white p-2 rounded">
+        Sign In with Google
+      </button>
+      <button onClick={handleSignOut} className="bg-red-500 text-white p-2 rounded mt-2">
+        Sign Out
+      </button>
+      <div></div>
     </div>
   );
 }
---
+++ libs/firebaseConfig.ts
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyABqMVx1IRuh94ToKZAqpaX4AUL_H8XoeM",
+  authDomain: "tutorial1-69e1d.firebaseapp.com",
+  projectId: "tutorial1-69e1d",
+  storageBucket: "tutorial1-69e1d.firebasestorage.app",
+  messagingSenderId: "1074960362083",
+  appId: "1:1074960362083:web:eea5b7cb52f57643242656",
+  measurementId: "G-04TD9WHQKN"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// Export firebase services
+export const auth = getAuth(app);
